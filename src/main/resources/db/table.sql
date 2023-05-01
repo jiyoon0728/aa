2f60dcf0da66d3b466e60434615147afabb37a1b
jiyoon <wldbf11111@naver.com>
2023-05-01 11:03:01 +0900

    db완료

--- src/main/resources/db/table.sql
+++ src/main/resources/db/table.sql
@@ -5,3 +5,21 @@ create table product(
     qty int not null, 
     createdAt timestamp not null
 );
+
+create table user_tb(
+    id int auto_increment primary key,
+    username varchar(100) not null unique,
+    password varchar not null,
+    email varchar(100) not null,
+    created_at timestamp
+);
+
+create table orders_tb(
+    id int primary KEY auto_increment,
+    orders_name varchar(20) NOT null,
+    orders_price int NOT null,
+    orders_qty int NOT null,
+    product_id int NOT null,
+    user_id int NOT null,
+    created_at timestamp
+);
